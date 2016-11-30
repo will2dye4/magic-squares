ec6202fe5cea34d05686743d2602958f7fc91aeb
William Dye <will2dye4@mac.com>
2016-11-29 20:44:09 -0500

    Added pre-checks and fixed bug with lucas-candidates.

--- src/magic_squares/core.clj
+++ src/magic_squares/core.clj
@@ -6,18 +6,20 @@
 ;; See: https://en.wikipedia.org/wiki/Magic_square#Method_for_constructing_a_magic_square_of_order_3
 (defn lucas-candidates 
   "Returns a seq of 3-item vectors [a b c] describing all candidates for
-  magic squares where c <= n using Lucas' method for squares of order 3."
+  magic squares where a + b + c <= n using Lucas' method for squares of order 3."
   [n]
+    {:pre [(pos? n)]}
     (for [a (range 1 (+ n 1))
-          b (range 1 (+ n 1))
-          c (range 1 (+ n 1))
-          :when (and (< a b (- c a)) (not= b (* 2 a)))]
+          b (range (+ a 1) (+ n 1))
+          c (range (+ a b 1) (+ n 1))
+          :when (and (< a b (- c a)) (not= b (* 2 a)) (<= (+ a b c) n))]
       [a b c]))
 
 (defn lucas-squares
   "Returns a lazy seq of 2D 3x3 vectors describing all magic squares for
   integers up to n (inclusive) using Lucas' method for squares of order 3."
   [n]
+    {:pre [(pos? n)]}
     (lazy-seq
       (for [[a b c] (lucas-candidates n) :let [d (- a b)]]
         [[(- c b) (+ a b c) (- c a)] [(- c d) c (+ c d)] [(+ a c) (- c (+ a b)) (+ b c)]])))
@@ -34,7 +36,9 @@
 
 (defn magic-number
   "Returns the magic number associated with the given 3x3 magic square."
-  [[[a b c] & _]] (+ a b c))
+  [[[a b c] & _] square]
+    {:pre [(check square)]}
+    (+ a b c))
 
 ;; Trying Matt Parker's "extra magic" 2-digit 3x3 squares, where
 ;; counting the letters in the numbers produces another magic square
@@ -58,9 +62,10 @@
   returns the letter count associated with n or zero if not found."
   [n counts] (or (some (fn [[count- nums]] (when (nums n) count-)) counts) 0))
  
-(defn count-letters  ;; assumption: 0 < n < 100
+(defn count-letters 
   "Returns the number of letters in the English spelling of the integer n."
   [n]
+    {:pre [(< 0 n 100)]}
     (if (< n 20)
       (-count n letter-counts)
       (let [tens (quot n 10) ones (mod n 10)]
